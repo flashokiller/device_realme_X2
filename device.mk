8342839959b1bc45d2dccbd61a029a0e0f39ef10
LuK1337 <priv.luk@gmail.com>
2019-11-30 22:10:58 +0530

    X2: Start using vendor_overlay
    * This lets us override files in /vendor without
      relying on bind mounts ^.^
    
    Change-Id: Ib0b173682a48836aeac97e2c3691176a44f13aab
    Signed-off-by: karthick111 <karthickrko61@gmail.com>

--- device.mk
+++ device.mk
@@ -53,9 +53,10 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/audio_platform_info_oppo_19771.xml:system/etc/audio_platform_info_oppo_19771.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_19771.xml:system/etc/mixer_paths_19771.xml
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info_oppo_19771.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_platform_info_oppo_19771.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_19771.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/mixer_paths_19771.xml
+
 
 # Bluetooth
 PRODUCT_PACKAGES += \
@@ -116,7 +117,7 @@ PRODUCT_COPY_FILES += \
 
 # TEMP-HOSTPAD
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/hostapd:system/bin/hw/hostapd
+    $(LOCAL_PATH)/configs/hostapd:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin/hw/hostapd
 
 # IMS
 PRODUCT_PACKAGES += \
