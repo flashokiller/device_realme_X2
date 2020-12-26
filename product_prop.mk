5802ca98f73b310ecd2740f96b2bec7ecb30009d
Vyom Desai <hypersync112@gmail.com>
2020-12-26 00:31:00 -0800

    X2: props: Disable blurs by default
    Keep access from Dev ops

--- product_prop.mk
+++ product_prop.mk
@@ -14,6 +14,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
     ro.sf.blurs_are_expensive=1 \
     debug.sf.latch_unsignaled=1 \
+    persist.sys.sf.disable_blurs=1 \
     debug.sf.disable_backpressure=1
 
 # Media
